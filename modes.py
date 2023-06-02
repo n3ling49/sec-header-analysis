9d51ee048ca28d90aeaaa045c31a74b8db1dcef0
n3ling49 <noahdreiling@gmail.com>
2023-06-02 12:45:27 +0200

    generalize multithreading and add saving of results in json file

--- modes.py
+++ modes.py
@@ -2,10 +2,10 @@ from seleniumwire import webdriver
 from selenium.webdriver.chrome.options import Options
 import utils
 import threading
+import math
 
-
-WEBSITEAMT = 30
-def scanWebsites(websites, amt = WEBSITEAMT):
+WEBSITEAMT = 6
+def scanWebsites(result, websites, amt = WEBSITEAMT):
     options = Options()
 
     # this parameter tells Chrome that
@@ -18,8 +18,11 @@ def scanWebsites(websites, amt = WEBSITEAMT):
     visited = 0
     while visited < amt:
         print(websites[visited].replace("\n", ""))
-        driver.get('https://www.' + utils.get(websites[visited]))
+        website = utils.get(websites[visited])
+        driver.get('https://www.' + website)
         print("Requests: " + str(len(driver.requests)))
+        result[website] = str(driver.requests)
+        del driver.requests
         """
         print(driver.requests[0].headers.keys())
         for header in driver.requests[0].headers.keys():
@@ -31,26 +34,24 @@ def scanWebsites(websites, amt = WEBSITEAMT):
         visited += 1
     driver.close()
 
-def multiScan(websites, threads):
-    #TODO: generalize
-    websitesPerThread = int(WEBSITEAMT/threads)
-    thread1 = threading.Thread(target=scanWebsites, args=(websites[:websitesPerThread], websitesPerThread,)) #will break if websiteamt is not dividible by threads
-    thread2 = threading.Thread(target=scanWebsites, args=(websites[websitesPerThread:2*websitesPerThread], websitesPerThread,))
-    thread3 = threading.Thread(target=scanWebsites, args=(websites[2*websitesPerThread:3*websitesPerThread], websitesPerThread,)) #will break if websiteamt is not dividible by threads
-    thread4 = threading.Thread(target=scanWebsites, args=(websites[3*websitesPerThread:4*websitesPerThread], websitesPerThread,))
-    thread5 = threading.Thread(target=scanWebsites, args=(websites[4*websitesPerThread:5*websitesPerThread], websitesPerThread,)) #will break if websiteamt is not dividible by threads
-    thread6 = threading.Thread(target=scanWebsites, args=(websites[5*websitesPerThread:], websitesPerThread,))
+"""
+Efficiently accesses websites included in 'websites' list by using 'threadAmt' amount of threads.
+Also saves every occurring https request per website in a dictionary, which the function then returns.
+"""
+def multiScan(websites, threadAmt):
+    result = dict()
 
-    thread1.start()
-    thread2.start()
-    thread3.start()
-    thread4.start()
-    thread5.start()
-    thread6.start()
+    threads = list()
+    websites_per_thread = math.ceil(int(WEBSITEAMT / threadAmt))
+    for i in range(0, threadAmt):
+        start_website_index = i * websites_per_thread
+        stop_website_index = (i + 1) * websites_per_thread
+        thread = threading.Thread(target=scanWebsites, args=(result, websites[start_website_index:stop_website_index], websites_per_thread))
+        threads.append(thread)
 
-    thread1.join()
-    thread2.join()
-    thread3.join()
-    thread4.join()
-    thread5.join()
-    thread6.join()
+    for thread in threads:
+        thread.start()
+
+    for thread in threads:
+        thread.join()
+    return result
--- utils.py
+++ utils.py
@@ -1,7 +1,12 @@
+import json
+
 def loadWebsites():
     with open('./resources/tranco_X5XLN.csv') as file:
         lines = file.readlines()
         return lines[20:]
-
 def get(website):
-    return website.split(",")[1]
+    return website.split(",")[1].replace("\n", "")
+
+def save(dict):
+    with open("results.json","w") as write_file:
+        json.dump(dict, write_file, indent=4)
