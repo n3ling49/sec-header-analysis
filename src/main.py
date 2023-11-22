c23aaeae0b09a83d3a4f4b48f1321c5b30f2f20f
n3ling49 <noahdreiling@gmail.com>
2023-11-22 22:05:04 +0100

    add config file

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pathlib
+import os
 import time
 import utils as utils
 import modes as modes
@@ -6,33 +7,35 @@ import script as script
 import requests
 from zipfile import ZipFile
 import clear_results as clear_results
+from config import CLEAR_RESULTS, CLEAR_RESULTS_EXCEPTIONS, CLEAR_PROCESSDATA, DOWNLOAD_TRANCO, PROCESS_AMT, WEBSITE_START, WEBSITE_END
 
-WEBSITEAMT = 100
 TRANCO_URL = "https://tranco-list.eu/top-1m.csv.zip"
-CLEAR = True
-CLEAR_EXCEPTIONS = []
 
 if __name__ == '__main__':
     # Clearing the results folder
-    if CLEAR:
+    if CLEAR_RESULTS:
         print("Clearing results folder")
-        clear_results.clear('../results/',CLEAR_EXCEPTIONS)
+        clear_results.clear('../results/',CLEAR_RESULTS_EXCEPTIONS)
+    if CLEAR_PROCESSDATA:
         print("Clearing processdata folder")
         clear_results.clear('../processdata/')
-    # Pulling the latest top 1 million websites list from Tranco
-    r = requests.get(TRANCO_URL, allow_redirects=True)
-    open('top-1m.csv.zip', 'wb').write(r.content)
-    with ZipFile('top-1m.csv.zip', 'r') as zipObj:
-        zipObj.extractall(path='/app/resources/')
-    # Loading a subset of websites from the list
+    if DOWNLOAD_TRANCO:
+        if pathlib.Path("/app/resources/top-1m.csv").exists():
+           os.remove("/app/resources/top-1m.csv") 
+        # Pulling the latest top 1 million websites list from Tranco
+        r = requests.get(TRANCO_URL, allow_redirects=True)
+        open('top-1m.csv.zip', 'wb').write(r.content)
+        with ZipFile('top-1m.csv.zip', 'r') as zipObj:
+            zipObj.extractall(path='/app/resources/')
+        # Loading a subset of websites from the list
 
-    websites = utils.loadWebsites(WEBSITEAMT)
+    websites = utils.loadWebsites(WEBSITE_END, WEBSITE_START)
     workingDir = pathlib.Path().resolve()
     utils.create_results_dir()
 
     #fetching the websites
     startTime2 = time.time()
-    result = modes.multiScan(websites, 10)
+    result = modes.multiScan(websites, PROCESS_AMT)
     stopTime2 = time.time()
     print("\nTime Multi: "+str(stopTime2-startTime2))
     script.getfailed(result)
